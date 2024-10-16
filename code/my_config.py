95ed014baf3d1fbf750574d2ee868f3f1fd972b1
eliasjiang <eliasjiang2002@gmail.com>
2024-10-16 14:27:12 +0800

    update

---
+++ code/my_config.py
@@ -0,0 +1,13 @@
+import numpy as np
+import torch
+import pandas as pd
+from torchvision import transforms,datasets
+import matplotlib.pyplot as plt
+
+from torch.utils.data import DataLoader, random_split,Dataset,Subset
+import torch.nn as nn
+
+# batch_size = 64
+# learning_rate = 0.001
+# num_epochs = 5
+
